e0aa028c4c8a4097e0eef5ca34d69ddae3c8fe14
icaPR <polosnramos@hotmail.com>
2022-10-12 02:08:13 -0300

    creat Header

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import { Header } from "../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início| YKNews</title>
+      </Head>
+    </>
+  );
+}
